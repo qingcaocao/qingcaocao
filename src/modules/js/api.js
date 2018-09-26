8bf242c1477df5149c18831c2de9815d2b5631ee
qingcaocao <359255564@qq.com>
2018-09-26 15:54:00 +0800

    完成首页及foot组建

---
+++ src/modules/js/api.js
@@ -0,0 +1,15 @@
+let url = {
+  hotlists: '/index/hotLists'
+}
+
+//开发环境和真实环境切换
+//let host= ''
+let host = 'http://rapapi.org/mockjsdata/23334'
+
+for (let key in url) {
+  if (url.hasOwnProperty(key)) {
+    url[key] = host + url [key]
+  }
+}
+
+export default url
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,15 +1,60 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'css/common.css'
+import './index.css'
+import 'css/common.css'
+import './index.css'
+
 import Vue from 'vue'
+import axios from 'axios'
+import url from 'js/api.js'
+import {InfiniteScroll,Button} from 'mint-ui'
+Vue.use(InfiniteScroll)
+Vue.component(Button.name, Button);
 
-// import router from '../../../router'
+import Foot from 'components/Foot.vue'
 
-Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  // router,
-  // components: { App },
-  // template: '<App/>'
+  el: '.vue-el',
+  data: {
+    lists: null,
+    pageNum: 1,
+    pageSize: 6,
+    loading: false,
+    allLoaded: false
+  },
+  created() {
+    this.getLists()
+  },
+  methods:{
+    getLists(){
+       //判断所有数据是否加载
+      if(this.allLoaded) return
+      //判断是否加载中
+      this.loading = true
+      //请求数据
+      axios.post(url.hotlists,{
+        pageNum: this.pageNum,
+        pageSize: this.pageSize
+      }).then(res=>{
+        console.log(res)
+        //判断数据是否加载完成
+        let currentLists = res.data.lists
+        if(currentLists.length < this.pageSize){
+          this.allLoaded = true
+        }
+        //拼接处理数据
+        if(this.lists) {
+          this.lists = this.lists.concat(currentLists)
+        } else{
+          this.lists = currentLists
+        }
+        //修改状态
+        this.loading = false
+        this.pageNum++
+      })
+    }
+  },
+  components: {
+    Foot
+  }
 })
