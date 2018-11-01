0ceae95f726a25f29abf07dada36d46bc8d5a1d1
qingcaocao <359255564@qq.com>
2018-11-01 10:40:05 +0800

    feat: 收货地址完成，路由配置完成

--- src/modules/js/fetch.js
+++ src/modules/js/fetch.js
@@ -1,23 +1,22 @@
 import axios from 'axios'
 import url from 'js/api.js'
-import { resolve } from 'path';
-import { rejects } from 'assert';
 
 function fetch(url,data) {
   return new Promise(
     (resolve,rejects) => {
-      axios.post(url,data).then(res => {
-        let status = res.data.status
-        if(status === 200) {
-          resolve(res)
-        }
-        if(status === 300) {
-          location.href = 'login.html'
-          resolve(res)
-        }
-        rejects(res)
+      axios.post(url,data).then( res => {
+        // let status = res.data.status
+        // if(status === 200) {
+        //   resolve(res)
+        // }
+        // if(status === 300) {
+        //   location.href = 'login.html'
+        //   resolve(res)
+        // }
+        // rejects(res)
+         resolve(res)
       }).catch(error => {
-        rejects(res)
+        rejects(error)
       })
     }
   )
